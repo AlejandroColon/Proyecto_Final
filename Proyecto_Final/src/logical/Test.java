d254bc5e440d8a3cbdbf63c00cadca6b9b91b644
Yamilka <Yamilka@Juancho>
2017-11-02 11:06:40 -0400

    Creando clase nueva

--- Proyecto_Final/src/logical/Test.java
+++ Proyecto_Final/src/logical/Test.java
@@ -1,29 +1,6 @@
 package logical;
 
 public class Test {
-	
-	private String nombre;
-	private String codigo;
-	private int edad;
-	
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	public int getEdad() {
-		return edad;
-	}
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
 
+	
 }
---
+++ Proyecto_Final/src/logical/Test2.java
@@ -0,0 +1,20 @@
+package logical;
+
+public class Test2 {
+
+	private String nombre1;
+	private String carrera;
+	public String getNombre1() {
+		return nombre1;
+	}
+	public void setNombre1(String nombre1) {
+		this.nombre1 = nombre1;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	
+}
