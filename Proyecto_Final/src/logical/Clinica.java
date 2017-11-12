e1658de88c67bbe08ecb9540f1a968c156e57eb9
OSCAR <OSCAR@OSCAR-PC>
2017-11-12 16:28:36 -0400

    Eliminacion de la clase Historial para que Yamilka la haga otra vez.

--- Proyecto_Final/src/logical/Clinica.java
+++ Proyecto_Final/src/logical/Clinica.java
@@ -6,14 +6,14 @@ public class Clinica {
 	
 	/***********************************ATRIBUTOS DE LA CLASE********************************/
 	private ArrayList<Persona> misPersonas;
-	private ArrayList<HistorialClinico> misHist;
+	
 	
 	
 	/************************************CONSTRUCTOR DE LA CLASE*****************************/
 	public Clinica() {
 		super();
 		misPersonas = new ArrayList<>();
-		misHist = new ArrayList<>();
+		
 	}
 	/****************************************GETS Y SETS*************************************/
 	public ArrayList<Persona> getMisPersonas() {
@@ -22,12 +22,7 @@ public class Clinica {
 	public void setMisPersonas(ArrayList<Persona> misPersonas) {
 		this.misPersonas = misPersonas;
 	}
-	public ArrayList<HistorialClinico> getMisHist() {
-		return misHist;
-	}
-	public void setMisHist(ArrayList<HistorialClinico> misHist) {
-		this.misHist = misHist;
-	}
+	
 
 	
 	
--- Proyecto_Final/src/logical/HistorialClinico.java
+++
@@ -1,7 +0,0 @@
-package logical;
-
-public class HistorialClinico {
-	
-	
-
-}
