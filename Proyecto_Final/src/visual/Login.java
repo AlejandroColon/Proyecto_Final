f3403213c1e973afe5ddbca5ce94c8df740c2d0a
OSCAR <OSCAR@OSCAR-PC>
2017-11-12 18:48:48 -0400

    Se agrega el paquete de imagenes y se crea la ventana para el login.

---
+++ Proyecto_Final/src/visual/Login.java
@@ -0,0 +1,100 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+
+public class Login extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -339133479221982755L;
+	/**
+	 * 
+	 */
+	
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtUsuario;
+	private JTextField txtContra;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Login dialog = new Login();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Login() {
+		setTitle("Acceso al usuario");
+		setBounds(100, 100, 265, 333);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setBounds(46, 157, 56, 14);
+		contentPanel.add(lblUsuario);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(46, 176, 157, 20);
+		contentPanel.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+		lblContrasea.setBounds(46, 201, 86, 14);
+		contentPanel.add(lblContrasea);
+		
+		txtContra = new JTextField();
+		txtContra.setBounds(46, 216, 157, 20);
+		contentPanel.add(txtContra);
+		txtContra.setColumns(10);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(63, 21, 125, 125);
+		contentPanel.add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/images/Users-User-Male-2-icon.png")));
+		panel.add(lblNewLabel, BorderLayout.CENTER);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Iniciar sesion");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
+/*
+ *CREADO POR: 
+ *				Oscar Rodriguez.
+ *				12/11/2017 
+ */
