a7510cb22ed9fa559ea211ce1874d1a9bc2ae11a
Alejandro <Alejandro@192.168.56.1>
2017-11-13 12:08:03 -0400

    creacion de ventana de registro enfermedades; intancia "Singleton" en clase "Clinica".

--- Proyecto_Final/src/logical/Clinica.java
+++ Proyecto_Final/src/logical/Clinica.java
@@ -2,14 +2,17 @@ package logical;
 
 import java.util.ArrayList;
 
+
 public class Clinica {
-	
-	//***********************************ATRIBUTOS********************************
+
+	// ***********************************ATRIBUTOS********************************
 	private ArrayList<Persona> misPersonas;
 	private ArrayList<Consulta> misConsultas;
 	private ArrayList<Vacuna> misVacunas;
 	private ArrayList<Enfermedad> misEnfermedades;
-	//************************************CONSTRUCTOR*****************************
+	private static Clinica clinica = null;
+
+	// ************************************CONSTRUCTOR*****************************
 	public Clinica() {
 		super();
 		misPersonas = new ArrayList<>();
@@ -17,49 +20,74 @@ public class Clinica {
 		misVacunas = new ArrayList<>();
 		misEnfermedades = new ArrayList<>();
 	}
-	//****************************************GETS Y SETS*************************************
+
+	// ****************************************GETS Y SETS*************************************
 	public ArrayList<Persona> getMisPersonas() {
 		return misPersonas;
 	}
+
 	public void setMisPersonas(ArrayList<Persona> misPersonas) {
 		this.misPersonas = misPersonas;
 	}
+
 	public ArrayList<Consulta> getMisConsultas() {
 		return misConsultas;
 	}
+
 	public void setMisConsultas(ArrayList<Consulta> misConsultas) {
 		this.misConsultas = misConsultas;
 	}
+
 	public ArrayList<Vacuna> getMisVacunas() {
 		return misVacunas;
 	}
+
 	public void setMisVacunas(ArrayList<Vacuna> misVacunas) {
 		this.misVacunas = misVacunas;
 	}
+
 	public ArrayList<Enfermedad> getMisEnfermedades() {
 		return misEnfermedades;
 	}
+
 	public void setMisEnfermedades(ArrayList<Enfermedad> misEnfermedades) {
 		this.misEnfermedades = misEnfermedades;
 	}
-	
-	//***************************************METODOS*******************************
-	
-	public void addPersona(Persona p){
+
+	// ***************************************METODOS*******************************
+	public static Clinica getInstance() {
+		if (clinica == null) {
+			clinica = new Clinica();
+		}
+		return clinica;
+	}
+
+	public void addPersona(Persona p) {
 		misPersonas.add(p);
 	}
 
-	public void addConsulta(Consulta c){
+	public void addConsulta(Consulta c) {
 		misConsultas.add(c);
 	}
-	
-	public void addEnfermedades(Enfermedad e){
+
+	public void addEnfermedades(Enfermedad e) {
 		misEnfermedades.add(e);
 	}
-	
-	public void addVacunas(Vacuna c){
+
+	public void addVacunas(Vacuna c) {
 		misVacunas.add(c);
 	}
-	
-	
+
 }
+
+/*
+ * Creado por: Alejandro Colón
+ * Fecha:
+ * Anotaciones:
+ * 
+ * 
+ * Modificacion: Alejandro Colón
+ * Fecha: 13/11/17
+ * Anotaciones: codificacion de instancia "Singleton"
+ * 
+ * */
---
+++ Proyecto_Final/src/visual/RegistrarEnfermedad.java
@@ -0,0 +1,172 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.JTextArea;
+import javax.swing.border.TitledBorder;
+
+import logical.Clinica;
+import logical.Enfermedad;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class RegistrarEnfermedad extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3139685405715440498L;
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtCodigo;
+	private JTextField txtNombre;
+	JTextArea txtDescripcion;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			RegistrarEnfermedad dialog = new RegistrarEnfermedad();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public RegistrarEnfermedad() {
+		setTitle("Registrar Enfermedad");
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(RegistrarEnfermedad.class.getResource("/images/icon.png")));
+		setBounds(100, 100, 330, 241);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(null, "Enfermedad Bajo Vigilancia", TitledBorder.LEADING, TitledBorder.TOP,
+					null, null));
+			panel.setBounds(10, 11, 302, 160);
+			contentPanel.add(panel);
+			panel.setLayout(null);
+			{
+				JLabel lblCdigo = new JLabel("C\u00F3digo:");
+				lblCdigo.setBounds(10, 21, 46, 14);
+				panel.add(lblCdigo);
+			}
+			{
+				txtCodigo = new JTextField();
+				txtCodigo.setHorizontalAlignment(SwingConstants.CENTER);
+				txtCodigo.setEditable(false);
+				txtCodigo.setBounds(10, 35, 95, 20);
+				panel.add(txtCodigo);
+				txtCodigo.setColumns(10);
+				txtCodigo.setText("EBV-" + (Clinica.getInstance().getMisEnfermedades().size() + 1));
+			}
+			{
+				txtNombre = new JTextField();
+				txtNombre.setColumns(10);
+				txtNombre.setBounds(155, 35, 138, 20);
+				panel.add(txtNombre);
+			}
+			{
+				JLabel lblNombre = new JLabel("Nombre:");
+				lblNombre.setBounds(155, 21, 67, 14);
+				panel.add(lblNombre);
+			}
+			{
+				JPanel panel_1 = new JPanel();
+				panel_1.setBounds(10, 81, 283, 69);
+				panel.add(panel_1);
+				panel_1.setLayout(new BorderLayout(0, 0));
+
+				JScrollPane scrollPane = new JScrollPane();
+				panel_1.add(scrollPane, BorderLayout.CENTER);
+				scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS); // Codigo
+																										// para
+																										// txtDescripcion
+																										// (textArea)
+				txtDescripcion = new JTextArea();
+				txtDescripcion.setLineWrap(true);
+				scrollPane.setViewportView(txtDescripcion);
+
+			}
+			{
+				JLabel lblDescripin = new JLabel("Descripi\u00F3n:");
+				lblDescripin.setBounds(10, 66, 67, 14);
+				panel.add(lblDescripin);
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnRegistrar = new JButton("Registrar");
+				btnRegistrar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if (txtCodigo.getText().equalsIgnoreCase("") && txtNombre.getText().equalsIgnoreCase("") && txtDescripcion.getText().equalsIgnoreCase("")) {
+							Enfermedad p = new Enfermedad(txtCodigo.getText(), txtNombre.getText(),
+									txtDescripcion.getText());
+							Clinica.getInstance().addEnfermedades(p);
+							JOptionPane.showMessageDialog(null, "Enfermedad agregada existosamente",
+									"Enfermedad Bajo Vigilancia", JOptionPane.INFORMATION_MESSAGE);
+							clear();
+						} else {
+							JOptionPane.showMessageDialog(null, "No pueden haber campos vacios", "ERROR",
+									JOptionPane.ERROR_MESSAGE);
+						}
+
+					}
+
+					public void clear() {
+						txtCodigo.setText("EBV-" + (Clinica.getInstance().getMisEnfermedades().size() + 1));
+						txtNombre.setText("");
+						txtDescripcion.setText("");
+					}
+
+				});
+				btnRegistrar.setActionCommand("OK");
+				buttonPane.add(btnRegistrar);
+				getRootPane().setDefaultButton(btnRegistrar);
+			}
+			{
+				JButton btnCancelar = new JButton("Cancelar");
+				btnCancelar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						txtNombre.setText("");
+						txtDescripcion.setText("");
+						setVisible(false);
+					}
+				});
+				btnCancelar.setActionCommand("Cancel");
+				buttonPane.add(btnCancelar);
+
+			}
+
+		}
+
+	}
+}
+
+/*
+ * Creado por: Alejandro Colón Fecha: 13/11/17 Anotaciones:
+ * 
+ */
